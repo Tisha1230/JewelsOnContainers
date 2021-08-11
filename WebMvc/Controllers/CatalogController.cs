ef76762e406e2b606b87b4fdadb6af8eee31e3d9
Tisha1230 <tishashrestha101@gmail.com>
2021-08-10 23:27:24 -0700

    Added views-catalog html

--- WebMvc/Controllers/CatalogController.cs
+++ WebMvc/Controllers/CatalogController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebMvc.Services;
+using WebMvc.ViewModels;
 
 namespace WebMvc.Controllers
 {
@@ -15,16 +16,36 @@ namespace WebMvc.Controllers
             _service = service;
         }
 
-        public async Task<IActionResult>  Index(int? page, int? brandFilterApplied, int? typesFilterApplied)
+        public async Task<IActionResult> Index(int? page, int? brandFilterApplied, int? typesFilterApplied)
         //this is the entry point from UI. Whatever user selects in view, these are the parameters that are going to come in as value
         {
             var itemsOnPage = 10; //hardcoding the pageSize.
 
             var catalog = await _service.GetCatalogItemsAsync(page ?? 0, itemsOnPage, brandFilterApplied, typesFilterApplied); //?? is simplified version of if statement for nullable check
 
+            var vm = new CatalogIndexViewModel
+            {
+                Brands = await _service.GetBrandsAsync(), //data for dropdown. making a service call to get the data
+                Types = await _service.GetTypesAsync(),
+                CatalogItems = catalog.Data,  //the middle section of the page
+                PaginationInfo = new PaginationInfo
+                {
+                    TotalItems = catalog.Count,
+                    ItemsPerPage = catalog.PageSize,
+                    ActualPage = page ?? 0, //if null then 0th page otherwise whatever page it is
+
+                    TotalPages = (int)Math.Ceiling((decimal)catalog.Count /itemsOnPage) 
+                                //Count is long value, itemsOnPage is an int. this will lose the round value when divided. eg: 33/10 = 3 pages but we need 4 pages
+                                //typecaste one of them, this case numerator into decimal, then Ceiling func will round it to next whole number
+                                //then converting it back to an int cuz total page is an int and result was
+                },
+
+                BrandFilterApplied = brandFilterApplied ?? 0, //whatever user selected, if it is null then set it to 0
+                TypesFilterApplied = typesFilterApplied ?? 0
+            };
 
 
-            return View();
+            return View(vm); //passing the data to the page
         }
     }
 }
---
+++ WebMvc/ViewModels/CatalogIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc.Models;
+
+namespace WebMvc.ViewModels
+{
+    public class CatalogIndexViewModel 
+    {
+        public IEnumerable<SelectListItem> Brands { get; set; } //brand dropdown
+        public IEnumerable<SelectListItem> Types { get; set; } //types dropdown
+        public IEnumerable<CatalogItem> CatalogItems { get; set; } //items displayed in page
+        public PaginationInfo PaginationInfo { get; set; } //page info
+
+        //tracks what the user had selected before
+        public int? BrandFilterApplied { get; set; }
+        public int? TypesFilterApplied { get; set; }
+    }
+}
